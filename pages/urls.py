1b064a569fe03e0c28b290eb6da26f6dc4d51299
victor2413garcia <vicmag2413@gmail.com>
2022-08-18 12:55:46 -0400

    new commit

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('juegos', views.juegos, name='juegos'),
+    path('contacto', views.contacto, name='contacto'),
+    path('recarga', views.recarga, name='recarga'),
+    path('retiro', views.retiro, name='retiro')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def juegos(request):
+    return render(request, 'pages/juegos.html')
+
+def contacto(request):
+    return render(request, 'pages/contacto.html')
+
+def recarga(request):
+    return render(request, 'pages/recarga.html')
+
+def retiro(request):
+    return render(request, 'pages/retiro.html')
